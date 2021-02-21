edcbed3ee7b8bf9285af6c120b196a3d3d0a4e30
Gabriel B <gabrielbicalho.eloi@gmail.com>
2021-02-21 15:10:50 -0300

    feat: Adicionando entidade company e configurando banco em memoria H2

---
+++ backend/src/main/java/com/techstar/TechstarApplication.java
@@ -0,0 +1,13 @@
+package com.techstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstarApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_company (name, location, description) VALUES ('Facebook', 'Menlo Park, Califórnia', 'Facebook, Inc. é um conglomerado estadunidense de tecnologia e mídia social com sede em Menlo Park, Califórnia.');
+INSERT INTO tb_company (name, location, description) VALUES ('Google', 'Mountain View, Califórnia', 'O Google hospeda e desenvolve uma série de serviços e produtos baseados na internet e gera lucro principalmente através da publicidade pelo AdWords. A Google é a principal subsidiária da Alphabet Inc.'); 
